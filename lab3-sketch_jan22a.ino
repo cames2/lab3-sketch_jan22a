6b6edacd43f651dfd501e7fe26965ed601345979
Spencer Dale <sdale26@uwo.ca>
2018-03-23 12:37:13 -0400

    Test Change (SD)

--- lab3-sketch_jan22a.ino
+++ lab3-sketch_jan22a.ino
@@ -1,7 +1,7 @@
 /*
   MSE 2202 MSEBot base code for Labs 3 and 4
   Language: Arduino
-  Authors: Michael Naish and Eugen Porter & Curtis Ames & Nick Baxter
+  Authors: Curtis Ames & Nick Baxter & Spencer Dale & Jenna Wemple
   Date: 16/01/17
   Rev 1 - Initial version
   Rev 2 - Update for MSEduino v. 2
